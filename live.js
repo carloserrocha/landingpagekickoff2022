3c3fda94f9aabdd07b008011fc79f10b92432f90
Carlos Eduardo Ramos da Rocha <68671643+carloserrocha@users.noreply.github.com>
2022-01-25 15:40:45 -0300

    V3.0

---
+++ live.js
@@ -0,0 +1,21 @@
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubePlayerAPIReady() {
+    player = new YT.Player('ytplayer', {
+        videoId: 'M7lc1UVf-VE',
+        playerVars: {
+            'autoplay': 1
+        },
+        events: {
+            'onReady': onPlayerReady,
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+}

13cf6f64d089ba40afff9340d84d7defb9f6cae1
Carlos Eduardo Ramos da Rocha <68671643+carloserrocha@users.noreply.github.com>
2022-01-25 10:30:08 -0300

    V1.10
    Dev testing of the code.

--- main.js
+++ main.js
@@ -1,26 +1,15 @@
-const second = 1000;
-const minute = second * 60;
-const hour = minute * 60;
-const day = hour * 24;
 
-let data_final = '01/27/2022' 
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-let count_down = new Date('01/27/2022 09:00:00').getTime();
-let x = setInterval(() => countDown(), second);
-
-function countDown() {
-  let now = new Date(Date.now()).getTime();
-  let diff = count_down - now;
-
-  document.getElementById('days').innerText = Math.floor(diff / day);
-  document.getElementById('hours').innerText = Math.floor(diff % day / hour);
-  document.getElementById('minutes').innerText = Math.floor(diff % hour / minute);
-  document.getElementById('seconds').innerText = Math.floor(diff % minute / second);
+var player;
+function onYouTubePlayerAPIReady() {
+    player = new YT.Player('ytplayer', {
+        videoId: 'M7lc1UVf-VE',
+        autoplay: '1'
+    });
 }
 
-function resetCountdown() {
-  clearInterval(x);
-  let date_end = data_final;
-  count_down = new Date(`${date_end} 09:00:00`).getTime();
-  x = setInterval(() => countDown(), second);
-}
+

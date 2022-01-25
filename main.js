cf7aafb1e47f827d28dd5eb960f39164c9544747
Carlos Eduardo Ramos da Rocha <68671643+carloserrocha@users.noreply.github.com>
2022-01-25 12:31:00 -0300

    Ambiente de testes V2.0
    Ambiente de testes para troca da layout

---
+++ main.js
@@ -0,0 +1,30 @@
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
+let data_final = '01/27/2022'
+
+let count_down = new Date('01/27/2022 9:00:00').getTime();
+let x = setInterval(() => countDown(), second);
+
+function countDown() {
+  let now = new Date(Date.now()).getTime();
+  let diff = count_down - now;
+
+  document.getElementById('days').innerText = Math.floor(diff / day);
+  document.getElementById('hours').innerText = Math.floor(diff % day / hour);
+  document.getElementById('minutes').innerText = Math.floor(diff % hour / minute);
+  document.getElementById('seconds').innerText = Math.floor(diff % minute / second);
+
+  if (diff < 1000) {
+    window.location.href = "https://dev.kickoff2022.teltecsolutions.com.br/teste"
+  }
+}
+
+function resetCountdown() {
+  clearInterval(x);
+  let date_end = data_final;
+  count_down = new Date(`${date_end} 09:00:00`).getTime();
+  x = setInterval(() => countDown(), second);
+}
